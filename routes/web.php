169e88e728db0475a10af8f3e7c52d37d9d45830
Antony Njoroge <antonjoro2008@gmail.com>
2025-09-23 15:09:54 +0300

    default route login added

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // Add a simple login route to prevent "Route [login] not defined" errors
-Route::get('/login', function () {
-    return redirect()->route('filament.admin.auth.login');
-})->name('login');
+// Route::get('/login', function () {
+//     return redirect()->route('filament.admin.auth.login');
+// })->name('login');
