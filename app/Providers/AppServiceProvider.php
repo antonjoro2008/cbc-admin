53416a0b0e507290a941d370ecf4a09496562077
Antony Njoroge <antonjoro2008@gmail.com>
2025-09-26 20:13:01 +0300

    default order by desc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
 use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Component;
 use Filament\Infolists\Components\TextEntry;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,12 +24,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Table::configureUsing(function (Table $table): void {
+            $table->defaultSort('id', 'DESC')
+                ->paginated([10, 25, 50, 100, 200, 500]);
+        });
+
         TextEntry::configureUsing(function (TextEntry $textEntry) {
             $textEntry->color('success');
         });
 
         DatePicker::configureUsing(function (DatePicker $component) {
             $component->displayFormat('d/m/Y');
+        });
+
+        Component::configureUsing(function (Component $component) {
+            if ($label = $component->getLabel()) {
+                $component->label(Str::title($label));
+            }
         });
     }
 }
