0199836b847b55602f695fb9acb3cd66fff8db1f
Antony Njoroge <antonjoro2008@gmail.com>
2026-05-20 17:14:07 +0300

    analytics optimization

--- resources/views/components/custom-styles.blade.php
+++ resources/views/components/custom-styles.blade.php
@@ -3,4 +3,11 @@
         height: 70px !important;
         margin: 5px;
     }
-</style><img src="{{ Storage::url('images/logo.png') }}" alt="Ismax" class="h-auto" width="70">
+
+    .fi-logo img {
+        width: 100% !important;
+        height: auto !important;
+        max-width: 100%;
+    }
+</style>
+<img src="{{ Storage::url('images/logo.png') }}" alt="Gravity CBC" class="h-auto">
